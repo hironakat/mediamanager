9e240b5271060b8410351e512d40239d45e19fad
hironakat <everlong1th@gmail.com>
2019-03-02 22:44:18 -0800

    jar

--- src/main/java/com/takashi/mediamanager/FileListUtils.java
+++ src/main/java/com/takashi/mediamanager/FileListUtils.java
@@ -48,14 +48,24 @@ public class FileListUtils extends FileList{
 
         ListIterator<FileInfo> fileInfoIterator = filelist.listIterator(0);
         ListIterator<FileInfo> fileInfoIteratorCheck;
-        List<String> targetFileNameVariation = new ArrayList<String>();
+        //List<String> targetFileNameVariation = new ArrayList<String>();
 
         while (fileInfoIterator.hasNext()) {
             FileInfo originalFile = fileInfoIterator.next();
             if(!originalFile.getNonPictureFile()) {
                 fileInfoIteratorCheck = filelist.listIterator(fileInfoIterator.nextIndex());
                 fileInfoIteratorCheck.forEachRemaining(targetFile -> {
-                    int pos = originalFile.getFileName().indexOf('.');
+                    if(originalFile.getFileSize() == targetFile.getFileSize()){
+                        try {
+                            if (Files.equal(originalFile.getFileObj(), targetFile.getFileObj())) {
+                                targetFile.setDuplicate(true);
+                                targetFile.setDuplicateOriginalFile(originalFile.getFileObj());
+                            }
+                        } catch (IOException e) {
+                            Utils.errPrint(e);
+                        }
+                    }
+                    /*int pos = originalFile.getFileName().indexOf('.');
                     StringBuilder sborg = new StringBuilder(originalFile.getFileName());
                     targetFileNameVariation.add(sborg.insert(pos, " copy").toString());
                     sborg = new StringBuilder(originalFile.getFileName());
@@ -85,7 +95,7 @@ public class FileListUtils extends FileList{
                             Utils.errPrint(e);
                         }
                     }
-                    targetFileNameVariation.clear();
+                    targetFileNameVariation.clear();*/
                 });
             }
             Utils.printProgress("duplicateCheck");
