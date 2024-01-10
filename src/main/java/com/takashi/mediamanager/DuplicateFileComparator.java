1a05b773fb30a2b9f79a85f6a4825ae067c5684f
hironakat <everlong1th@gmail.com>
2024-01-10 08:19:50 -0800

    update

--- src/main/java/com/takashi/mediamanager/DuplicateFileComparator.java
+++ src/main/java/com/takashi/mediamanager/DuplicateFileComparator.java
@@ -7,11 +7,14 @@ public class DuplicateFileComparator implements Comparator<DuplicateFiles>{
     public int compare(DuplicateFiles p1, DuplicateFiles p2) {
         if (p1 == null) {
             p1 = new DuplicateFiles("orgFile error","orgFile error");
-        }else if(p2 == null){
+        }
+        if(p2 == null){
             p1 = new DuplicateFiles("dupFile error","dupFile error");
-        }else if(p1.getOrginalFile() == null){
+        }
+        if(p1.getOrginalFile() == null){
             p1.setFiles("orgFile error","orgFile error");
-        }else if(p2.getDuplicateFile() == null){
+        }
+        if(p2.getDuplicateFile() == null){
             p2.setFiles("orgFile error","dupFile error");
         }
         return p1.getOrginalFile().compareTo(p2.getDuplicateFile());
